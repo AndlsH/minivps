8c7d2f99b15988fb99d87494bdd2ce9ffb565923
Andls <cnhanw@gmail.com>
2017-03-04 17:43:53 +0800

    SSH port setting

--- vpsinit.sh
+++ vpsinit.sh
@@ -4,6 +4,20 @@ Welcome()
 {
     clear
 }
+}
+SetSshPort()
+{
+    echo "Please input SSH port:"
+    read -p "(Default port: 2048):" sshPort
+    [ -z "${sshPort}" ] && sshPort="2048"
+    echo
+    echo "---------------------------"
+    echo "  SSH port = ${sshPort}"
+    echo "---------------------------"
+    echo
+    sed -i "/Port 22/aPort ${sshPort}" /etc/ssh/sshd_config
+    systemctl restart sshd
+}
 action=$1
 [ -z $1 ] && action=Welcome
 ${action}
